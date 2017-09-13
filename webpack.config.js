d415c349c0780cb835b932b48d56576f8b9d201e
Morgan Wesemann <morgan.wesemann@gmail.com>
2017-09-12 19:09:02 -0500

    Another pipeline fix

--- webpack.config.js
+++ webpack.config.js
@@ -50,7 +50,7 @@ if(env === 'dev') {
 		hot: true,
 		port: port,
 		https: true,
-		contentBase: htmlRoot,
+		contentBase: dest,
 		publicPath: 'https://localhost:' + port + '/',
 		headers: {
 			'access-control-allow-origin': '*'
