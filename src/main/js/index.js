8f42ac9ed2c25df9839983f9dd7fc8933519fbf6
Ejay Mallard <ejaymallard@yahoo.com>
2017-10-11 15:59:38 -0500

    - ejay petsitter

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -7,6 +7,7 @@ import {Login} from 'js/pages/Login';
 import {Search} from 'js/pages/Search';
 import {Detail} from 'js/pages/Detail';
 import {Booking} from 'js/pages/Booking';
+import {Sitter} from 'js/pages/SitterDemo';
 import {Payment} from 'js/pages/Payment';
 import {Help} from 'js/pages/Help';
 import {Contact} from 'js/pages/Contact';
@@ -25,6 +26,7 @@ export default class Index extends React.Component {
 					<Route exact path="/booking" component={Booking} />
 					<Route exact path="/payment" component={Payment} />
 					<Route exact path="/help" component={Help} />
+					<Route exact path="/sitter" component={Sitter} />
 				</div>
 			</HashRouter>
 		);
