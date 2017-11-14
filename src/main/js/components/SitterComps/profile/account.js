06b802d573d4f190f13e5918354c8e0b5c671240
Annie Mathis <annie_mathis@baylor.edu>
2017-11-14 13:01:27 -0600

    working on getting username from database

--- src/main/js/components/SitterComps/profile/account.js
+++ src/main/js/components/SitterComps/profile/account.js
@@ -1,64 +1,136 @@
+import React from "react";
+import { Grid, Row, Col, Panel, Image } from "react-bootstrap";
+import { NavBar } from "js/components/SitterComps/navbar/navbar.js";
+import { Sider } from "js/components/SitterComps/sidebar/sidebar.js";
+import { StatusForm } from "js/components/SitterComps/main/statusform.js";
+import { PetList } from "js/components/PetList.js";
+import { FileInput } from "react-file-input";
+import { BookMe } from "js/components/SitterComps/bookme/bookme.js";
+import { StarRating } from "js/components/SitterComps/rate/StarRating.js";
+import { RateMe } from "js/components/SitterComps/rate/rateme.js";
 
-import React from 'react';
-import { Grid, Row, Col, Panel, Image} from 'react-bootstrap';
-import { NavBar } from 'js/components/SitterComps/navbar/navbar.js';
-import { Sider } from 'js/components/SitterComps/sidebar/sidebar.js';
-import { StatusForm } from 'js/components/SitterComps/main/statusform.js';
-import { PetList } from 'js/components/PetList.js';
-import {FileInput } from 'react-file-input';
-import { BookMe } from 'js/components/SitterComps/bookme/bookme.js';
-import { StarRating } from 'js/components/SitterComps/rate/StarRating.js';
-import { RateMe } from 'js/components/SitterComps/rate/rateme.js';
 export class Account extends React.Component {
+  constructor(props) {
+    super(props);
 
-    render(){
+    this.state = {
+      userToken: this.getCookie("usertoken"),
+      userId: this.getCookie("userid")
+    };
+  }
 
-        return (
-                <div className="row">
-                        <div className="col-md-2 hidden-xs" align="center">
-                              <Image src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png" responsive />
-                            <div>
-                                <label >
-                                    <div className="inputWrapper">
-                                    </div>
-                                </label>
-                            </div>
-                        </div>
+  getUserName() {
 
-                        <div className="col-md-9 col-xs-9">
-                            <h1><i>Ejay Mallard</i></h1>
-                            <table className="table table-user-information">
-                                <tbody>
-                                <tr>
-                                    <td><h1>Waco, TX </h1></td>
-                                    <tr>
-                                      <td><h1><BookMe/></h1></td>
-                                      <td><h1><RateMe /></h1></td>
-                                    </tr>
-                                </tr>
-                                </tbody>
-                            </table>
-                            < br />
-                            <h2><i>Personal Information</i></h2>
-                            <table>
-                                <tbody>
-                                <tr>
-                                    <td><h3>Pay Rate: </h3></td>
-                                    <td>< br/> &nbsp;&nbsp;&nbsp;&nbsp;$12          </td>
-                                </tr>
-                                <tr>
-                                    <td><h3>Classification: </h3></td>
-                                    <td>< br/> &nbsp;&nbsp;&nbsp;&nbsp;Sitter       </td>
-                                </tr>
-                                <tr>
-                                    <td><h3>Rating: </h3></td>
-                                    <td>< br/> &nbsp;&nbsp;&nbsp;&nbsp;0 of 5 Stars             </td>
-                                </tr>
-                                </tbody>
-                            </table>
+    var config = {
+      headers: { Authorization: "Bearer " + this.state.userToken }
+    };
 
-                        </div>
-                    </div>
-        );
+    const url = "https://group-3-tempeturs-backend.herokuapp.com/api";
+
+    axios
+      .get(url + "/user/" + this.state.userId)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(function(error) {
+        alert("error!");
+        console.log(error);
+      });
+  }
+
+  getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == " ") {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
     }
+    return "";
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <div className="col-md-2 hidden-xs" align="center">
+          <Image
+            src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"
+            responsive
+          />
+          <div>
+            <label>
+              <div className="inputWrapper" />
+            </label>
+          </div>
+        </div>
+
+        <div className="col-md-9 col-xs-9">
+          <h1 onClick={this.getUserName}>
+            <i>Ejay Mallard</i>
+          </h1>
+
+          <table className="table table-user-information">
+            <tbody>
+              <tr>
+                <td>
+                  <h1>Waco, TX </h1>
+                </td>
+                <tr>
+                  <td>
+                    <h1>
+                      <BookMe />
+                    </h1>
+                  </td>
+                  <td>
+                    <h1>
+                      <RateMe />
+                    </h1>
+                  </td>
+                </tr>
+              </tr>
+            </tbody>
+          </table>
+
+
+          <br />
+          <h2>
+            <i>Personal Information</i>
+          </h2>
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <h3>Pay Rate: </h3>
+                </td>
+                <td>
+                  <br /> &nbsp;&nbsp;&nbsp;&nbsp;$12{" "}
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <h3>Classification: </h3>
+                </td>
+                <td>
+                  <br /> &nbsp;&nbsp;&nbsp;&nbsp;Sitter{" "}
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <h3>Rating: </h3>
+                </td>
+                <td>
+                  <br /> &nbsp;&nbsp;&nbsp;&nbsp;0 of 5 Stars{" "}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
